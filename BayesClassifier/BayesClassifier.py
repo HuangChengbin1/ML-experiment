3bf00ad1acc447df93fea4965107c6e89eaa46aa
Corsini <103578745+HuangChengbin1@users.noreply.github.com>
2024-04-25 21:37:09 +0800

    upload BayesClassifier

---
+++ BayesClassifier/BayesClassifier.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+
+class BayesClassifier:
+
+    def __init__(self):
+        self.X = None
+        self.y = None
+        self.classes = None
+        self.parameters = None
+
+    def naive_bayes_classifier(self, train_data, test_data):
+        labels = train_data.iloc[:, -1].value_counts().index    # type of labels
+        mean = []   # mean for per class
+        var = []    # variance for per class
+        res = []    # prediction for test data
+
+        for i in labels:
+            item = train_data.loc[train_data.iloc[:, -1] == i, :]   # extract each class
+            m = item.iloc[:, :-1].mean()    # mean for cur class
+            v = np.sum((item.iloc[:, :-1] - m)**2) / (item.shape[0])    # variance for cur class
+            mean.append(m)
+            var.append(v)
+
+        means = pd.DataFrame(mean, index=labels)
+        vars = pd.DataFrame(var, index=labels)
+
+        for j in range(test_data.shape[0]):
+            iset = test_data.iloc[j, :].tolist()  # cur sample
+            iprob = np.exp(-1 * (iset - means)**2 / (vars*2)) / (np.sqrt(2 * np.pi) * vars)     # normal distribution
+            prob = 1
+            for k in range(test_data.shape[1] - 1):
+                prob *= iprob[k]
+                cla = prob.index[np.argmax(prob.values)]    # class with greatest prob
+            res.append(cla)
+        test_data['predict'] = res
+        return test_data
---
+++ BayesClassifier/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+filepath = "D:\\机器学习\\实验\\实验6\\ch07+ch08\\ch07研讨+实验\\data.txt"
+sep = '\t'
+
+data = pd.read_csv(filepath, sep=sep, header=None)
+size_mapping = {'S': 1, 'M': 2, 'L': 3}
+data.iloc[:, 1] = data.iloc[:, 1].map(size_mapping)
+print("train_data after mapping: \n", data)
+
+test_x = pd.DataFrame({0: 2, 1: 'S'}, index=[0])
+test_x.iloc[:, 1] = test_x.iloc[:, 1].map(size_mapping)
+print("test_data after mapping: \n", test_x)
+
+from BayesClassifier import BayesClassifier
+bc = BayesClassifier()
+test = bc.naive_bayes_classifier(data, test_x)
+print("test_data with predict result: \n", test)
+
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(data.iloc[:, :-1], data.iloc[:, -1])
+pred_x = clf.predict(test_x.iloc[:, :-1])
+print("predict result by sklearn.naive_bayes GaussionNB: \n", pred_x)
